e4c13da4a057b0bfff5ca0026ecd8d9b1d2dd76f
Mateus Correia <momnc16@gmail.com>
2024-04-24 15:00:08 +0100

    feat - Automatic packet capture
    All packets that pass through any of the switch's ports are captured and recorded on a dedicated file, for later analysis.
    
    I chose to you tcpdump instead of wireshark, because it allows for this kind of automation.

---
+++ Shared Folder/PIC_main.py
@@ -0,0 +1,27 @@
+import subprocess
+import threading
+import time
+
+def run_tcpdump(interface, output_file):
+    def target():
+        time.sleep(5)  # Delay the start of tcpdump
+        subprocess.Popen(['tcpdump', '-i', interface, '-w', output_file])
+    threading.Thread(target=target).start()
+
+def run_script(script_path):
+    def target():
+        subprocess.Popen(['python', script_path])
+    threading.Thread(target=target).start()
+
+# Call PIC_Layout.py
+run_script('PIC_Layout.py')
+
+# Start two tcpdump instances after a delay
+time.sleep(10)  # Wait for the script to start
+print('tcpdump -D:')
+subprocess.Popen(['tcpdump', '-D'])
+run_tcpdump('s1-eth1', 'output1.pcap')
+run_tcpdump('s1-eth2', 'output2.pcap')
+run_tcpdump('s1-eth3', 'output3.pcap')
+run_tcpdump('s1-eth4', 'output4.pcap')
+run_tcpdump('s1', 'output5.pcap')
