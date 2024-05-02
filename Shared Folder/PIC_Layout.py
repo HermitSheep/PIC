da7b51129f6fbd1acd3cf9c611c4a3362249265f
Mateus Correia <momnc16@gmail.com>
2024-05-02 09:55:52 +0100

    feat - Link script to external controller
    Instead of using a locally created controller, the AP's and switch are now using the ONOS controller.

--- Shared Folder/PIC_Layout.py
+++ Shared Folder/PIC_Layout.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 
-from mininet.node import Controller, OVSKernelSwitch, Host
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController, OVSKernelSwitch, UserSwitch, CPULimitedHost, Host, Node
 from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf, OVSLink
+
 from mn_wifi.net import Mininet_wifi
 from mn_wifi.node import Station, OVSKernelAP
 from mn_wifi.cli import CLI
 from mn_wifi.link import wmediumd
 from mn_wifi.wmediumdConnector import interference
+
 from subprocess import call
 
 import sys
@@ -17,23 +21,13 @@ from mn_wifi.replaying import ReplayingMobility
 
 def myNetwork(args):
 
-
-    net = Mininet_wifi(topo=None,
-                       build=False,
-                       link=wmediumd,
-                       wmediumd_mode=interference,
-                       ipBase='10.0.0.0/8')
+    net = Mininet_wifi(topo=None, build=False, link=OVSLink, ipBase='10.0.0.0/8')
 
     info( '*** Adding Nodes\n' )
-    c1 = net.addController(name='c1',
-                           controller=Controller,
-                           protocol='tcp',
-                           port=6633)
-    ap1 = net.addAccessPoint('ap1', cls=OVSKernelAP, listenPort=6633, ssid='ap1-ssid',
-                             channel='1', mode='g', ip='10.0.1.0', position='23.0,23.0,0', range='23')
-    ap2 = net.addAccessPoint('ap2', cls=OVSKernelAP, listenPort=6633, ssid='ap2-ssid',
-                             channel='1', mode='g', ip='10.0.2.0', position='46.0,23.0,0', range='23')
-    s1 = net.addSwitch('s1', cls=OVSKernelSwitch, listenPort=6633)
+    c1 = net.addController(name='c1', controller=RemoteController, ip='172.17.0.2', protocol='tcp', port=6653)
+    ap1 = net.addAccessPoint('ap1', cls=OVSKernelAP, protocols=["OpenFlow10"], ssid='ap1-ssid', channel='1', mode='g', ip='10.0.1.0', position='23.0,23.0,0', range='23')
+    ap2 = net.addAccessPoint('ap2', cls=OVSKernelAP, protocols=["OpenFlow10"], ssid='ap2-ssid', channel='1', mode='g', ip='10.0.2.0', position='46.0,23.0,0', range='23')
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch, protocols=["OpenFlow10"])
     sta1 = net.addStation('sta1', ip='10.0.0.1', speed=1, position='23.0,33.0,0', range='23')
     h1 = net.addHost('h1', cls=Host, ip='10.0.0.2', defaultRoute=None)
     h2 = net.addHost('h2', cls=Host, ip='10.0.0.3', defaultRoute=None)
@@ -59,7 +53,8 @@ def myNetwork(args):
     
     info( '*** Starting network\n')
     net.build()
-    c1.start()
+    for controller in net.controllers:
+        controller.start()
     ap1.start([c1])
     ap2.start([c1])
     s1.start([c1])
